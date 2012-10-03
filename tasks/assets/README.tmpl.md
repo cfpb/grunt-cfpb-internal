a0bff86adc591b99c0224d74b0dda774012bfaf4
Chris Contolini <christopher.contolini@cfpb.gov>
2012-10-03 12:43:08 -0500

    Large rewrite for CFPB use

---
+++ tasks/assets/README.tmpl.md
@@ -0,0 +1,28 @@
+{% _.each(docs, function(doc, name) { %}
+{%= doc %}
+{% }); %}
+
+## Release History
+{% if (changelog) {
+  _.each(changelog, function(details, version) {
+    var date = details.date;
+    if (date instanceof Date) {
+      date = grunt.template.date(new Date(date.getTime() + date.getTimezoneOffset() * 60000), 'yyyy-mm-dd');
+    }
+    print('\n * ' + [
+      date,
+      version,
+      details.changes.join(' '),
+    ].join('\u2003\u2003\u2003'));
+  });
+} else { %}
+_(Nothing yet)_
+{% } %}
+
+## License
+
+{%= license || 'This project is a work of the public domain.' %}
+
+---
+
+*This file was generated on {%= grunt.template.today() %}.*
---
+++ tasks/build-contrib.js
@@ -0,0 +1,43 @@
+/*
+ * grunt-contrib-internal
+ * http://gruntjs.com/
+ *
+ * Copyright (c) 2012 Tyler Kellen, contributors
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Add custom template delimiters.
+  grunt.template.addDelimiters('build-contrib', '{%', '%}');
+
+  grunt.registerTask('build-cfpb', 'Generate CFPB README', function() {
+
+    var path = require('path'),
+        fs = require('fs'),
+        asset = path.join.bind(null, __dirname, 'assets'),
+        meta = grunt.file.readJSON('package.json');
+
+    meta.changelog = grunt.file.readYAML('CHANGELOG.md');
+    meta.license = grunt.file.read('LICENSE');
+
+    grunt.file.expand('docs/*.md').forEach( function(filepath) {
+      var filename = path.basename( filepath, '.md' );
+      meta.docs[filename] = grunt.file.read( filepath );
+    });
+
+    // Generate readme.
+    var tmpl = grunt.file.read( asset('README.tmpl.md') ),
+        appendix = grunt.template.process( tmpl, {data: meta, delimiters: 'build-contrib'} ),
+        readme = grunt.file.exists('README.md') ? grunt.file.read('README.md') : '';
+
+    grunt.file.write( 'README.md', readme + appendix );
+    grunt.log.ok('Created README.md');
+
+    // Fail task if any errors were logged.
+    if ( this.errorCount > 0 ) { return false; }
+  });
+
+};
